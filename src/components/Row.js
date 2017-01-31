11869b818ee052c8044c090f43f133f6cf0fcfd1
Mitchell Rose <mmrose@cogdata.com>
2017-01-31 16:40:56 -0500

    enable row level toggle

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,22 +1,103 @@
 import React from 'react'
+import uuid from 'uuid'
 
 // assigned to component after component definition, no hoisting within
 const defaultProps = {
     className: ''
-  , label: 'row need label pls'
+  , label: 'row need default label pls'
   , expand: true
   , expandCell: true
-  , toggleRow: null
   , title: ''
   , cols: '2'
 }
 
 export default class Row extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state =  { expand: props.expand }
+  }
+
+  componentWillMount() {
+    this.uuid = uuid.v4()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.expand !== this.props.expand) {
+      if (this.props.expand) {
+//        this.maybeExpand();
+//      } else {
+//        this.handleCollapse();
+      }
+    }
+  }
+
+  handleClick = () => {
+console.log( 'expand is: ' + this.state.expand )
+console.log( 'cols: ' + this.props.cols )
+
+    this.setState( previousState => ({
+      expand: !previousState.expand
+    }))
+
+//    this.toggleTarget(target, this.toggleSource(src))
+  }
+
+// {...this.getRowProps()}
+  getRowProps = () => {
+    let props = {
+      key:this.uuid
+    }
+    return props
+  }
+
+  getLabelColProps = () => {
+    let props = {
+        className: this.props.className
+      , key: uuid.v4()
+      , title: this.props.title
+      , onClick: this.handleClick
+    }
+    if (!this.state.expand) {
+      props.style = {fontStyle:'italic'}
+    }
+    return props
+  }
+
+  getDataColProps = () => {
+    let props = {
+        className:'reactdump-data'
+      , key: uuid.v4()
+    }
+    if (!this.state.expand) {
+      props.style = {display:'none'}
+    }
+    return props
+  }
+
+  getCols = () => {
+    let arrCols = [ ]
+    if (this.props.cols === '2') {
+      arrCols.push(<td {...this.getLabelColProps()}>{this.props.label}</td>)
+    }
+    arrCols.push(<td {...this.getDataColProps()}>{this.props.children}</td>)
+    return arrCols
+  }
 
   render() {
+    return (
+      <tr {...this.getRowProps()}>
+        {this.getCols()}
+      </tr>
+    )
+  }
+
+/*
+  render() {
+    console.log( 'expand is: ' + this.state.expand )
+    const expd = this.state.expand ? '' : `style={display:'none'}`
     switch (this.props.cols) {
       case '1':
-        if (this.props.expand) {
+        if (this.state.expand) {
           return (
             <tr>
               <td className="reactdump-data">
@@ -34,19 +115,22 @@ export default class Row extends React.Component {
         )
 
       default:
-        if (this.props.expand) {
+
+        if (this.state.expand) {
           return (
             <tr>
-              <td className={this.props.className} title={this.props.title} onClick={this.props.toggleRow}>{this.props.label}</td>
+              <td className={this.props.className} title={this.props.title} onClick={this.handleClick}>{this.props.label}</td>
               <td className="reactdump-data">
                 {this.props.children}
               </td>
             </tr>
           )
         }
+
         return (
+        // collapsed
           <tr style={{display:'none'}}>
-            <td className={this.props.className} style={{fontStyle: 'italic'}} title={this.props.title} onClick={this.props.toggleRow}>{this.props.label}</td>
+            <td className={this.props.className} style={{fontStyle: 'italic'}} title={this.props.title} onClick={this.handleClick}>{this.props.label}</td>
             <td className="reactdump-data" style={{display:'none'}}>
               {this.props.children}
             </td>
@@ -56,6 +140,6 @@ export default class Row extends React.Component {
     } // /switch
 
   }
-
+*/
 }
 Row.defaultProps = defaultProps
