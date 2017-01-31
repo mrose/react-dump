869cac1a3d249eab5acb44a249db4c3e5b1ae749
Mitchell Rose <mmrose@cogdata.com>
2017-01-31 16:41:16 -0500

    add some comments

--- src/components/getPathToCircularRef.js
+++ src/components/getPathToCircularRef.js
@@ -5,7 +5,15 @@
  * @param {object} cache
  * @param {array} currentPath
  * @returns {Array}
+ *
+ * cache = {
+ *   bFilteredLevel: false
+ *   , level:0
+ *   , objects: []
+ *   , paths: []
+ *   }
  */
+
 const getPathToCircularRef = (obj, cache, currentPath) => {
     let circPath = []
 
@@ -14,8 +22,9 @@ const getPathToCircularRef = (obj, cache, currentPath) => {
     }
 
     let pos = cache.objects.indexOf(obj)
-    if (pos >= 0) {
+    if (pos !== -1) {
       circPath = cache.paths[pos]
+      return circPath
     }
 
     cache.objects.push(obj)
