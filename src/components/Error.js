2374c113f4976e7a419721271a1023f926c517bf
Mitchell Rose <mmrose@cogdata.com>
2017-01-31 17:04:56 -0500

    complete removal of RowHeader in favor of Table

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Table from './Table'
 import Row from './Row'
-import RowHeader from './RowHeader'
 
 // assigned to component after component definition, no hoisting within
 const defaultProps = {
@@ -15,8 +14,7 @@ const defaultProps = {
 export default class Error extends React.Component {
   render() {
     return (
-      <Table className='reactdump reactdump-Error' >
-        <RowHeader className='reactdump-label reactdump-Error' label={this.props.opts.label} expand={this.props.opts.expand} cols='1' />
+      <Table className='reactdump reactdump-Error' label={this.props.opts.label} cols='1'>
         <Row className='reactdump-label reactdump-Error' label={this.props.opts.label} expand={this.props.opts.expand} expandCells={this.props.opts.expand}>
           {this.props.obj.toString()}
         </Row>
--- src/components/RegExp.js
+++ src/components/RegExp.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Table from './Table'
 import Row from './Row'
-import RowHeader from './RowHeader'
 
 const defaultProps = {
     obj: null
@@ -14,8 +13,7 @@ const defaultProps = {
 export default class RegExp extends React.Component {
   render() {
     return (
-      <Table className='reactdump reactdump-RegExp' >
-        <RowHeader dataType='RegExp' label={this.props.opts.label} expand={this.props.opts.expand} cols='1' />
+      <Table className='reactdump reactdump-RegExp' label={this.props.opts.label} cols='1'>
         <Row className='reactdump-label reactdump-RegExp' label={this.props.opts.label} expand={this.props.opts.expand} expandCells={this.props.opts.expand}>
         {this.props.obj.toString()}
         </Row>
